81576acb6b8ae056e4fbf4e5abd6b47ad865d23c
Kevin <NateraKevin1@gmail.com>
2022-06-27 11:15:31 -0400

    updated models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,13 @@
 class Like < ApplicationRecord
+    validates :liker_id, :post_id, presence: true
+
+    belongs_to :liker,
+        primary_key: :id,
+        foreign_key: :liker_id,
+        class_name: :User
+
+    belongs_to :post,
+        primary_key: :id,
+        foreign_key: :post_id,
+        class_name: :Post
 end
