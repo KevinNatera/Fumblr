5fa02984f58181f188113d72429259dc278e8f83
Kevin <NateraKevin1@gmail.com>
2022-06-26 21:59:01 -0400

    preparing to merge

--- frontend/components/posts/create_post.jsx
+++ frontend/components/posts/create_post.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom';
-import PostsIndexContainer from '../posts/posts_index_container';
+
 
 class CreatePost extends React.Component {
     constructor(props){
         super(props)
+
+       
     }
         
     componentDidMount(){
-        
+
     }
+
 
     render() {
         return (
             <div className="create-post-frame">
                
-              <Link className="btn" to="/new/text">Text</Link>
-          
+              <Link className="btn" to="/new/text" onClick={this.props.openCreateTextPost}>Text</Link>
+            {/* <button className="btn"onClick={this.props.openCreateTextPost} > TEST</button> */}
              
 
               <Link className="btn" to="/new/photo">Photo</Link>
