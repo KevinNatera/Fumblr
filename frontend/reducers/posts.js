063aacde8d077521ecddff3ef29b602af4d5eaba
Kevin <NateraKevin1@gmail.com>
2022-06-27 01:04:05 -0400

    fixed delete

--- frontend/reducers/posts.js
+++ frontend/reducers/posts.js
@@ -3,14 +3,13 @@ import { RECEIVE_ALL_POSTS, RECEIVE_POST , REMOVE_POST} from '../actions/posts';
 const postsReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
-    
   case RECEIVE_ALL_POSTS:
     return Object.assign( {}, action.posts, state);
     case RECEIVE_POST:
       const newPost = { [action.post.id]: action.post };
       return newPost;
     case REMOVE_POST:
-      
+      let nextState = Object.assign({}, state);
       delete nextState[action.postId];
       return nextState;
   default:
