2a7c5e23df629e4abbbc762f6ffabb5ec99ce725
Kevin <NateraKevin1@gmail.com>
2022-06-30 00:15:22 -0400

    styled create and edit post form

--- frontend/components/users/user_show_nav.jsx
+++ frontend/components/users/user_show_nav.jsx
@@ -1,29 +1,15 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-
-
-
-
-
-const profileRef = React.createRef()
-
- 
-      
-        
+    
 export default function UserShowNav() {
     
     const location = useLocation();
-    console.log(location)
-
-    
-       
-
+   
         return (
-
 
             <div className="user-show-nav-bar">
                 
-                <Link className={location.pathname === "/profile" ? "selected" : "btn"} name="profile" ref={profileRef} to="/profile" >Posts</Link>
+                <Link className={location.pathname === "/profile" ? "selected" : "btn"} to="/profile" >Posts</Link>
                 <Link className={location.pathname === "/followers" ? "selected" : "btn"} to="/followers">Followers</Link>
                 <Link className={location.pathname === "/activity" ? "selected" : "btn"} to="/activity">Activity</Link>
                 <Link className={location.pathname === "/edit-profile" ? "selected" : "btn"}to="/edit-profile">Edit Appearance</Link>
